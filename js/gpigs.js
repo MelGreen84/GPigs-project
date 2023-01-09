2b561a15a6b894bb8697c2a586eca61a81806a3f
MelGreen84 <Melgreen84@gmail.com>
2023-01-09 06:45:12 +0000

    Image edits

--- js/gpigs.js
+++ js/gpigs.js
@@ -1,14 +1,14 @@
 // Short-haired breed array
-gpigssh = [["American", "The most popular breed of guinea pig. They are suitable for new piggie owners because their short hair makes them easy to maintain.", "<img src='img/sh/american_guinea_pig.jpg' class = 'breedStyle img '/>"],
-["Dalmation", "A sweet, short-haired guinea pig whose hair is primarily white with black spots. They are suitable for beginners because they don't need alot of grooming.", "<img src='img/sh/dalmation_gp.jpg' class = 'breedStyle img '/>"],
-["Teddy", "Course, short hair that doesn't need much grooming. Teddy guinea pigs are known for being one of the most relaxed and friendly breeds.", "<img src='img/sh/teddy.jpg' class = 'breedStyle img'/>"],
-["Agouti", "The Agouti is a smooth-coated guinea pig with a very cute face. They are suitable for beginners because they don't need alot of grooming.", "<img src='img/sh/agouti.jpg' class = 'breedStyle img '/>"]];
+gpigssh = [["American", "The most popular breed of guinea pig. They are suitable for new piggie owners because their short hair makes them easy to maintain.", "<img src='img/sh/american_guinea_pig.jpg' class = 'breedImg'/>"],
+["Dalmation", "A sweet, short-haired guinea pig whose hair is primarily white with black spots. They are suitable for beginners because they don't need alot of grooming.", "<img src='img/sh/dalmation_gp.jpg'class = 'breedImg'/>"],
+["Teddy", "Course, short hair that doesn't need much grooming. Teddy guinea pigs are known for being one of the most relaxed and friendly breeds.", "<img src='img/sh/teddy.jpg'class = 'breedImg'/>"],
+["Agouti", "The Agouti is a smooth-coated guinea pig with a very cute face. They are suitable for beginners because they don't need alot of grooming.", "<img src='img/sh/agouti.jpg'class = 'breedImg'/>"]];
 
 // Function
 function breedsh(breedName) {
     switch (breedName) {
         case ("American"):
-            document.getElementById("breed").innerHTML = "Breed: " + gpigssh[0][0] + "<br>" + "Description: " + gpigssh[0][1] + "<br>" + gpigssh[0][2];
+            document.getElementById("breed").innerHTML = "Breed: " + gpigssh[0][0] + "<br>" + "Description: " + gpigssh[0][1] + gpigssh[0][2];
             document.getElementById("breed").style.backgroundColor = "#0000ff80";
             break;
         case ("Dalmation"):
@@ -28,10 +28,10 @@ function breedsh(breedName) {
 }
 
 // Long-haired breed array
-gpigslh = [["Peruvian", "They have the longest fur out of all guinea pig breeds and therefore require regular grooming by experienced guinea pig owners.", "<img src='img/lh/peruvian.png' class = 'breedStyle img'/>"],
-["Abyssinian", "These little piggies are friendly, cheeky and love to play. Their hair stands on end because it grows in rosettes.", "<img src='img/lh/abyssinian_guinea_pig.jpg' class = 'breedStyle img'/>"],
-["Sheltie", "Also known as Silkies, they have long, soft, silky fur that requires regular grooming. Suitable for experienced guinea pig owners.", "<img src='img/lh/sheltie_guinea_pig.jpg' class = 'breedStyle img'/>"],
-["Coronet", "Similar to Shelties, with long, silky, soft hair, they are not suitable for beginners because they require regular grooming.", "<img src='img/lh/coronet_guinea_pig.jpg' class = 'breedStyle img'/>"]];
+gpigslh = [["Peruvian", "They have the longest fur out of all guinea pig breeds and therefore require regular grooming by experienced guinea pig owners.", "<img src='img/lh/peruvian.png' class = 'breedImg'/>"],
+["Abyssinian", "These little piggies are friendly, cheeky and love to play. Their hair stands on end because it grows in rosettes. Suitable for fairly experienced guinea pig owners because they require some grooming.", "<img src='img/lh/abyssinian_guinea_pig.jpg' class = 'breedImg'/>"],
+["Sheltie", "Also known as Silkies, they have long, soft, silky fur that requires regular grooming. Suitable for experienced guinea pig owners.", "<img src='img/lh/sheltie_guinea_pig.jpg' class = 'breedImg'/>"],
+["Coronet", "Similar to Shelties, with long, silky, soft hair, they are not suitable for beginners because they require regular grooming.", "<img src='img/lh/coronet_guinea_pig.jpg' class = 'breedImg'/>"]];
 
 // Function
 function breedlh(breedName) {
